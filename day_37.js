2cae8dfeaa72ce54d48afdf5463ea121e0abc339
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-10-12 13:05:41 -0700

    Linked List: insertLast

--- day_37.js
+++ day_37.js
@@ -15,13 +15,37 @@ class LinkedList {
     this.head = new Node(data, this.head)
     this.size++;
   }
+  // Insert last node
+  insertLast(data) {
+    let node = new Node(data);
+    let current;
+
+    // If empty, make head
+    if(!this.head){
+      this.head = node;
+    } else {
+      current = this.head;
+
+      while(current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
 }
 
 let my_list = new LinkedList()
-console.log(my_list);
+// console.table(my_list);
 
 my_list.insertFirst("chicken");
-console.log(my_list);
+// console.table(my_list);
 
 my_list.insertFirst("cow");
-console.log(my_list);
+console.log(JSON.stringify(my_list));
+
+my_list.insertLast("pig");
+console.log(JSON.stringify(my_list));
+
+my_list.insertLast("horse");
+console.log(JSON.stringify(my_list));
