f72f5840574365d27a3439b77d5c64db4a302c1f
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2021-10-21 17:13:36 -0700

    Understanding destructuring in JS

---
+++ destructuring-practice.js
@@ -0,0 +1,15 @@
+const person = {
+    firstName: 'Bob',
+    lastName: 'Marley',
+    email: 'bob@marley.com',
+    password: 'sekureP@ssw0rd9',
+    username: 'barley',
+    createdAt: 1543945177623
+};
+const animals = ['horse', 'dog', 'fish', 'cat', 'bird'];
+
+// DESTRUCTURING
+const { email } = person;
+const [firstAnimal] = animals;
+console.log(email); // =>bob@marley.com
+console.log(firstAnimal); // =>horse
